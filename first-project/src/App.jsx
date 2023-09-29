a90b6f0e7ffc88ede568427bd3ca54be8cf53a4f
judiciousmurich <murichjudicious45@gmail.com>
2023-09-29 17:38:30 +0300

    feat:add navbar styles

--- first-project/src/App.jsx
+++ first-project/src/App.jsx
@@ -1,9 +1,12 @@
+import Navbar from "./components/Navbar"
 
 function App() {
 
   return (
     <>
-
+      <div>
+        <Navbar />
+      </div>
 
     </>
   )
---
+++ first-project/src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='w-full'>REACT.</div>
+  )
+}
+
+export default Navbar
