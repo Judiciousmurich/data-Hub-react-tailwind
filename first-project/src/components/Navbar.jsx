8533402dac7403e4ab53a5f411165f769f80fce4
judiciousmurich <murichjudicious45@gmail.com>
2023-09-30 13:52:37 +0300

    feat:add navbar styles

--- first-project/src/components/Navbar.jsx
+++ first-project/src/components/Navbar.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-
+import {AiOutlineMenu } from "react-icons/ai";
 const Navbar = () => {
     return (
-        <div className='flex  justify-between items-center h-24 max-w-[1240px] mx-auto' >
+        <div className='flex  justify-between items-center h-24 max-w-[1240px] mx-auto px-4' >
             <h1 className='w-full text-3xl font-bold text-[#00df9a]'>REACT.</h1>
-            <ul className='flex gap-4'>
+            <ul className='flex hidden'>
                 <li className='p-4'>Home</li>
                 <li className='p-4'>Company</li>
                 <li className='p-4'>Resources</li>
@@ -13,6 +13,18 @@ const Navbar = () => {
 
 
             </ul>
+            <div>
+                <AiOutlineMenu size={20}/>
+            </div>
+            <div>
+                <ul>
+                <li className='p-4'>Home</li>
+                <li className='p-4'>Company</li>
+                <li className='p-4'>Resources</li>
+                <li className='p-4'>About</li>
+                <li className='p-4'>Contact</li>
+                </ul>
+            </div>
         </div>
     )
 }
