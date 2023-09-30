a23fb337212382038d13d78842895ed29b3a683d
judiciousmurich <murichjudicious45@gmail.com>
2023-09-30 15:03:54 +0300

    feat:add navbar state

--- first-project/src/components/Navbar.jsx
+++ first-project/src/components/Navbar.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
-import { AiOutlineMenu } from "react-icons/ai";
+import React, { useState } from 'react'
+import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 const Navbar = () => {
+    const [nav, setNav] = useState(false)
+
+    const handleNav = () => {
+        setNav(!nav)
+
+    }
     return (
         <div className='flex  justify-between items-center h-24 max-w-[1240px] mx-auto px-4' >
-             <h1 className='w-full text-3xl font-bold text-[#00df9a] '>REACT.</h1>
+             <h1 className='w-full text-3xl font-bold text-[#36e1ab] '>REACT.</h1>
             <ul className='flex hidden'>
                 <li className='p-4'>Home</li>
                 <li className='p-4'>Company</li>
@@ -13,10 +19,10 @@ const Navbar = () => {
 
 
             </ul>
-            <div>
-                <AiOutlineMenu size={20} />
+            <div onClick={handleNav}>
+                {!nav ? <AiOutlineClose size={20}/> : <AiOutlineMenu size={20}/>}
             </div>
-            <div className='fixed left-0 top-0 w-[60%] h-full border-r border-gray-900 bg-[#000300]'>
+            <div className={!nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-gray-900 bg-[#000300] ease-in-out duration-500' : 'fixed left[-100%]' }>
             <h1 className='w-full text-3xl font-bold text-[#00df9a] m-4'>REACT.</h1>
 
                 <ul className=' uppercase p-4'>
