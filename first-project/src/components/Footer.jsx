1ffdded1be42db5949126f52b7e724105c617145
judiciousmurich <murichjudicious45@gmail.com>
2023-10-02 21:50:24 +0300

    featt:add analytcs

--- first-project/src/components/Footer.jsx
+++ first-project/src/components/Footer.jsx
@@ -20,7 +20,8 @@ const Footer = () => {
             <FaGithubSquare size={30} />
             <FaDribbbleSquare size={30} />
         </div>
-        <div className='lg:col-span-2 flex justify-between mt-6'>
+      </div>
+      <div className='lg:col-span-2 flex justify-between mt-6'>
     <div>
         <h6 className='font-medium text-gray-400'>Solutions</h6>
         <ul>
@@ -30,3 +31,36 @@ const Footer = () => {
             <li className='py-2 text-sm'>Insights</li>
         </ul>
     </div>
+    <div>
+        <h6 className='font-medium text-gray-400'>Support</h6>
+        <ul>
+            <li className='py-2 text-sm'>Pricing</li>
+            <li className='py-2 text-sm'>Documentation</li>
+            <li className='py-2 text-sm'>Guides</li>
+            <li className='py-2 text-sm'>API Status</li>
+        </ul>
+    </div>
+    <div>
+        <h6 className='font-medium text-gray-400'>Company</h6>
+        <ul>
+            <li className='py-2 text-sm'>About</li>
+            <li className='py-2 text-sm'>Blog</li>
+            <li className='py-2 text-sm'>Jobs</li>
+            <li className='py-2 text-sm'>Press</li>
+            <li className='py-2 text-sm'>Careers</li>
+        </ul>
+    </div>
+    <div>
+        <h6 className='font-medium text-gray-400'>Legal</h6>
+        <ul>
+            <li className='py-2 text-sm'>Claim</li>
+            <li className='py-2 text-sm'>Policy</li>
+            <li className='py-2 text-sm'>Terms</li>
+        </ul>
+    </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
