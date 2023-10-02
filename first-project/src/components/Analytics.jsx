ebc9e1bbcfe3afa3d9a947c60e06537239204482
judiciousmurich <murichjudicious45@gmail.com>
2023-10-02 21:24:56 +0300

    featt:add analytcs

--- first-project/src/components/Analytics.jsx
+++ first-project/src/components/Analytics.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const Analytics = () => {
   return (
-    <div>Analytics</div>
+      <div className='w-full bg-white py-16 px-4'>
+      <div className='max-w-[1240px] mx-auto grid md:grid-cols-2'>
+        <img className='w-[500px] mx-auto my-4' src={Laptop} alt='/' />
+        <div className='flex flex-col justify-center'>
+          <p className='text-[#00df9a] font-bold '>DATA ANALYTICS DASHBOARD</p>
+          <h1 className='md:text-4xl sm:text-3xl text-2xl font-bold py-2'>Manage Data Analytics Centrally</h1>
+          <p>
   )
 }
 
