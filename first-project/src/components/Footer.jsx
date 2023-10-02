55662bad605ee3d0b6a129d8fc58b1dddf4cbe76
judiciousmurich <murichjudicious45@gmail.com>
2023-10-02 21:44:55 +0300

    featt:add analytcs

--- first-project/src/components/Footer.jsx
+++ first-project/src/components/Footer.jsx
@@ -20,3 +20,13 @@ const Footer = () => {
             <FaGithubSquare size={30} />
             <FaDribbbleSquare size={30} />
         </div>
+        <div className='lg:col-span-2 flex justify-between mt-6'>
+    <div>
+        <h6 className='font-medium text-gray-400'>Solutions</h6>
+        <ul>
+            <li className='py-2 text-sm'>Analytics</li>
+            <li className='py-2 text-sm'>Marketing</li>
+            <li className='py-2 text-sm'>Commerce</li>
+            <li className='py-2 text-sm'>Insights</li>
+        </ul>
+    </div>
