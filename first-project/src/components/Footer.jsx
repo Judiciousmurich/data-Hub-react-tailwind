c515026318ba04c1567240c19c295d530a824bf2
judiciousmurich <murichjudicious45@gmail.com>
2023-10-02 21:39:22 +0300

    featt:add analytcs

--- first-project/src/components/Footer.jsx
+++ first-project/src/components/Footer.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from 'react';
+import {
+  FaDribbbleSquare,
+  FaFacebookSquare,
+  FaGithubSquare,
+  FaInstagram,
+  FaTwitterSquare,
+} from 'react-icons/fa';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
-
-export default Footer
+    <div className='max-w-[1240px] mx-auto py-16 px-4 grid lg:grid-cols-3 gap-8 text-gray-300'>
+      <div>
+        <h1 className='w-full text-3xl font-bold text-[#00df9a]'>REACT.</h1>
+        <p className='py-4'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Id odit ullam iste repellat consequatur libero reiciendis, blanditiis accusantium.</p>
+        <div className='flex justify-between md:w-[75%] my-6'>
+            <FaFacebookSquare size={30} />
+            <FaInstagram size={30} />
+            <FaTwitterSquare size={30} />
+            <FaGithubSquare size={30} />
+            <FaDribbbleSquare size={30} />
+        </div>
