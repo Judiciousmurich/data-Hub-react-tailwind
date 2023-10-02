7c062076a6941f85a1d89c915fc6a593eee05a5c
judiciousmurich <murichjudicious45@gmail.com>
2023-10-02 21:30:53 +0300

    featt:add analytcs

--- first-project/src/components/Analytics.jsx
+++ first-project/src/components/Analytics.jsx
@@ -15,6 +15,9 @@ const Analytics = () => {
             eveniet ex deserunt fuga?
           </p>
                     <button className='bg-black text-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto md:mx-0 py-3'>Get Started</button>
+                    </div>
+      </div>
+    </div>
   )
 }
 
