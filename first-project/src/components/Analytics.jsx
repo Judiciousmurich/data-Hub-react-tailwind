c3f5c374fd9b81f59412842f3e350a776498ff5d
judiciousmurich <murichjudicious45@gmail.com>
2023-10-03 15:40:33 +0300

    feat:add analytics styles

--- first-project/src/components/Analytics.jsx
+++ first-project/src/components/Analytics.jsx
@@ -7,9 +7,10 @@ export const Analytics = () => {
       <div className='max-w-[1240px ] mx-auto grid md:grid-cols-2'>
         <img className='w-[500px] mx-auto my-4' src={Laptop} alt="/" />
         <div className='flex flex-col justify-center'>
-          <p className='text-[#00df9a] font-bold'>DATA ANALYTICS DASHBOARD</p>
-          <h1>Manage Data Analytics Centrally</h1>
+          <p className='text-[rgb(0,223,154)] font-bold'>DATA ANALYTICS DASHBOARD</p>
+          <h1 className='md:text-4xl sm:text-3xl text-2xl font-bold py-2'>Manage Data Analytics Centrally</h1>
           <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nam a doloribus vitae, facilis cupiditate pariatur soluta et saepe? Illo fugiat doloremque natus sed voluptas quia? Laudantium quibusdam sit voluptatem nostrum!</p>
+          <button className='bg-black text-[rgb(0,223,154)'>Get Started</button>
         </div>
 
       </div>
