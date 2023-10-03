e784fdb061de20eb11fef7d3906c987af5e2f000
judiciousmurich <murichjudicious45@gmail.com>
2023-10-03 16:15:17 +0300

    feat:add newsletter  styles

--- first-project/src/components/Newsletter.jsx
+++ first-project/src/components/Newsletter.jsx
@@ -4,9 +4,18 @@ const Newsletter = () => {
   return (
     <div className='w-full py-16 text-white px-4'>
       <div className='max-w-[1240px] mx-auto grid lg:grid-cols-3'>
-        <div>
-          <h1> Want tips & tricks to optimize your flow?</h1>
+        <div className='lg:col-span-2 my-4'>
+          <h1 className='md:text-4xl sm:text-3xl text-2xl font-bold  py-2'> Want tips & tricks to optimize your flow?</h1>
           <p>Sign up to our newsletter and stay up to date.</p>
+        </div>
+        <div>
+          <div>
+            <input  type='email'
+              placeholder='Enter Email'/>
+              <button className='bg-[rgb(0,223,154)] text-black rounded-md font-medium w-[200px] ml-4 my-6 px-6 py-3'>
+              Notify Me
+            </button>
+          </div>
         </div>
 
       </div>
